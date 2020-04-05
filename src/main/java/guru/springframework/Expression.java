dec295f0919c2135961b38be73f64f6049aa5d85
Vinicius Silva <viniciussilva@tokenlab.com.br>
2020-04-05 16:34:12 -0300

    Abstraction

--- src/main/java/guru/springframework/Expression.java
+++ src/main/java/guru/springframework/Expression.java
@@ -4,4 +4,6 @@ public interface Expression {
     Money reduce(Bank bank, String to);
 
     public Expression plus(Expression addend);
+
+    Expression times(int multiplier);
 }
--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -35,6 +35,7 @@ public class Money implements Expression {
                 '}';
     }
 
+    @Override
     public Expression times(int multiplier) {
         return new Money(amount * multiplier, this.currency);
     }
