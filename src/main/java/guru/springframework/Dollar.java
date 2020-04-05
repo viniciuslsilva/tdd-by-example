b01292d26f54180dcf2dd9a243f536010a63f0b0
Vinicius Silva <viniciussilva@tokenlab.com.br>
2020-04-05 13:30:57 -0300

    Adding Dollar and test

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,14 @@
+package guru.springframework;
+
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public void times(int multiplier) {
+        amount *= multiplier;
+    }
+}
