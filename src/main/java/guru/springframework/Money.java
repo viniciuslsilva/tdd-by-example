386130a6ae2d96d4d36efa78c0c0f2a0fdc65f38
Vinicius Silva <viniciussilva@tokenlab.com.br>
2020-04-05 16:30:48 -0300

    Mixed currencies

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -35,11 +35,12 @@ public class Money implements Expression {
                 '}';
     }
 
-    public Money times(int multiplier) {
+    public Expression times(int multiplier) {
         return new Money(amount * multiplier, this.currency);
     }
 
-    public Expression plus(Money addend){
+    @Override
+    public Expression plus(Expression addend){
         return new Sum(this, addend);
     }
 
