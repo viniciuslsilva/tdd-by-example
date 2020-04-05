86fd2f5e7ba79ecf4b0b3fed6010b5d00ab32664
Vinicius Silva <viniciussilva@tokenlab.com.br>
2020-04-05 16:01:35 -0300

    Refactored times and removed subclasses

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,6 +1,6 @@
 package guru.springframework;
 
-public abstract class Money {
+public class Money {
     protected int amount;
     protected String currency;
 
@@ -13,19 +13,29 @@ public abstract class Money {
         return currency;
     }
 
-    public abstract Money times(int multiplier);
-
     public static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     public static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
         return amount == money.amount
-                && this.getClass().equals(object.getClass());
+                && this.currency == money.currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, this.currency);
     }
 }
